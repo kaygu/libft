2b0c8ed3f5034f5293dc105ae8c99936f5732c32
Camille DE NEEF <cde-neef@student.42.fr>
2016-11-12 15:04:25 +0100

    run ft_lstdel recursively

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: cde-neef <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/08 16:19:16 by cde-neef          #+#    #+#             */
-/*   Updated: 2016/11/10 19:42:58 by cde-neef         ###   ########.fr       */
+/*   Updated: 2016/11/12 14:58:45 by cde-neef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,11 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
-	t_list	*alst_next;
-
-	while (*alst != NULL)
+	if (*alst != NULL)
 	{
-		alst_next = (*alst)->next;
+		ft_lstdel(&(*alst)->next, del);
 		(*del)((*alst)->content, (*alst)->content_size);
 		free(*alst);
-		*alst = alst_next;
 	}
 	*alst = NULL;
 }
